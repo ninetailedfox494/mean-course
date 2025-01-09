efe48a519aa7231f84b59f4203beae9a847f6c70
Thuy Doan <dev.thuydoan@gmail.com>
2025-01-09 22:21:24 +0700

    add tailwind

---
+++ angular-mean-couse/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss}", // Include Angular files (HTML, TypeScript, SCSS)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
